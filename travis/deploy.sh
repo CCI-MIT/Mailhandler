5fbdd5b503dd33ac112dfeeeed69ffa221168af3
Johannes Bachhuber <jobachhu@mit.edu>
2018-06-07 01:35:49 +0200

    Align travis deploy script with xCoLab

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,9 +4,12 @@ eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
 
+WORKER_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
 DEPLOY_SERVER=binaries@cognosis2.mit.edu
-echo "Copying binaries to ${DEPLOY_SERVER}:mailhandler ..."
-scp target/mailhandler-*.jar ${DEPLOY_SERVER}:mailhandler/
+DEPLOY_FOLDER=mailhandler
+echo "Copying binaries from ${WORKER_IP} to ${DEPLOY_SERVER}:${DEPLOY_FOLDER} ..."
+scp target/mailhandler-*.jar ${DEPLOY_SERVER}:${DEPLOY_FOLDER}/
 
 # Workaround for hanging builds
 # taken from https://github.com/travis-ci/travis-ci/issues/8082#issuecomment-315147561

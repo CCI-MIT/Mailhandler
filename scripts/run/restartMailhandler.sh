70f34123afffae3f385a8561c7668d712dc940f0
Johannes Bachhuber <jobachhu@mit.edu>
2018-06-07 01:47:54 +0200

    Fix restart script

--- scripts/run/restartMailhandler.sh
+++ scripts/run/restartMailhandler.sh
@@ -11,16 +11,16 @@ if [ -f ${PID_FILE} ]; then
     echo "[INFO] Sending kill signal to ${BASE_FILENAME}"
     kill $(<"${PID_FILE}")
     rm ${PID_FILE}
-else
-    JAR_NAME=${BASE_FILENAME}-${VERSION}.jar
-    echo "[INFO] Downloading jar from binaries server..."
-    scp binaries@cognosis2.mit.edu:${JAR_NAME} ./
-
-    OUT_FILE="${BASE_FILENAME}.out"
-    rm ${OUT_FILE} > /dev/null 2>&1
-    echo "[INFO] Starting jar..."
-    exec java -jar ${BASE_FILENAME}-${VERSION}.jar > ${OUT_FILE}  & echo $! > ${PID_FILE}
 fi
+
+JAR_NAME=${BASE_FILENAME}-${VERSION}.jar
+echo "[INFO] Downloading jar from binaries server..."
+scp binaries@cognosis2.mit.edu:mailhandler/${JAR_NAME} ./
+
+OUT_FILE="${BASE_FILENAME}.out"
+rm ${OUT_FILE} > /dev/null 2>&1
+echo "[INFO] Starting jar..."
+exec java -jar ${BASE_FILENAME}-${VERSION}.jar > ${OUT_FILE}  & echo $! > ${PID_FILE}
 
 echo "[INFO] Done."
 echo "#####################################################################################"
